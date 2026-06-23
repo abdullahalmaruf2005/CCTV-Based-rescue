a6621334514b4338a667c1b262d50b13f859299e
SadiaShuvra <souvra.cse10.bu@gmail.com>
2026-06-23 22:04:45 +0600

    Add initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,2 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+
+export default function App() {
+  const [running, setRunning] = useState(false);
+  const [fire, setFire] = useState(false);
+
+  const startCamera = async () => {
+    await axios.get("http://localhost:5000/start_camera");
+    setRunning(true);
+    checkStatus();
+  };
